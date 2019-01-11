cdad625cd80dd23729002bc31153d789f4583ffe
Robert Laws <roblaws@me.com>
2019-01-11 16:59:39 +0300

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,23 @@ var browserSync = require("browser-sync").create();
 var paths = {
   styles: {      
     src: "src/styles/**/*.scss",
-    dest: "dist/styles"
+    dest: "dist/styles",
+    prod: "styles"
   },
   fonts: {
     src: ["src/fonts/**/*.css", "src/fonts/**/*.eot", "src/fonts/**/*.svg", "src/fonts/**/*.ttf", "src/fonts/**/*.woff"],
-    dest: "dist/styles"
+    dest: "dist/styles",
+    prod: "styles"
   },
   html: {
     src: "src/html/**/*.html",
-    dest: "dist"
+    dest: "dist",
+    prod: ""
   },
   images: {
     src: ["src/images/**/*.jpg", "src/images/**/*.png", "src/images/**/*.ico"],
-    dest: "dist/images"
+    dest: "dist/images",
+    prod: "images"
   }
 };
 
@@ -54,6 +58,38 @@ gulp.task("images", function() {
     .pipe(browserSync.stream());
 });
 
+
+gulp.task("htmlProd", function() {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.prod))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("fontsProd", function() {
+  return gulp.src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.prod))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("sassProd", function () {
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass(
+      {
+        outputStyle: "expanded"
+      })
+    .on("errorProd", sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styles.prod))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("imagesProd", function() {
+  return gulp.src(paths.images.src)
+    .pipe(gulp.dest(paths.images.prod))
+    .pipe(browserSync.stream());
+});
+
 gulp.task("clean", function () {
   return del(["dist"]);
 });
@@ -67,6 +103,8 @@ gulp.task("watch", function () {
   gulp.watch(["src/styles/**/*.scss", "src/html/**/*.html"], ["html", "fonts", "images", "sass"]);
 });
 
-gulp.task("build", ["html", "fonts", "sass", "images"]);
+gulp.task("dev", ["html", "fonts", "sass", "images"]);
+
+gulp.task("build", ["htmlProd", "fontsProd", "sassProd", "imagesProd"]);
 
 gulp.task("default", ["sass", "html", "fonts", "images", "watch"]);
