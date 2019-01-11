6454bc6d82130ce3d236acc7733227aa154c9dba
Robert Laws <roblaws@me.com>
2019-01-11 17:36:14 +0300

    update gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require("gulp");
+var gulpif = require("gulp-if");
 var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
 var del = require("del");
 var browserSync = require("browser-sync").create();
+
+var argv = require('yargs').argv;
+var isProduction = (argv.production === undefined) ? false : true;
 	
 var paths = {
   styles: {      
@@ -29,13 +33,15 @@ var paths = {
 
 gulp.task("html", function() {
   return gulp.src(paths.html.src)
-    .pipe(gulp.dest(paths.html.dest))
+    .pipe(gulpif(isProduction, gulp.dest(paths.html.dest)))
+    .pipe(gulpif(!isProduction, gulp.dest(paths.html.prod)))
     .pipe(browserSync.stream());
 });
 
 gulp.task("fonts", function() {
   return gulp.src(paths.fonts.src)
-    .pipe(gulp.dest(paths.fonts.dest))
+    .pipe(gulpif(isProduction, gulp.dest(paths.fonts.dest)))
+    .pipe(gulpif(!isProduction, gulp.dest(paths.fonts.prod)))
     .pipe(browserSync.stream());
 });
 
@@ -48,45 +54,15 @@ gulp.task("sass", function () {
       })
     .on("error", sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(gulpif(isProduction, gulp.dest(paths.styles.dest)))
+    .pipe(gulpif(!isProduction, gulp.dest(paths.styles.prod)))
     .pipe(browserSync.stream());
 });
 
 gulp.task("images", function() {
   return gulp.src(paths.images.src)
-    .pipe(gulp.dest(paths.images.dest))
-    .pipe(browserSync.stream());
-});
-
-
-gulp.task("htmlProd", function() {
-  return gulp.src(paths.html.src)
-    .pipe(gulp.dest(paths.html.prod))
-    .pipe(browserSync.stream());
-});
-
-gulp.task("fontsProd", function() {
-  return gulp.src(paths.fonts.src)
-    .pipe(gulp.dest(paths.fonts.prod))
-    .pipe(browserSync.stream());
-});
-
-gulp.task("sassProd", function () {
-  return gulp.src(paths.styles.src)
-    .pipe(sourcemaps.init())
-    .pipe(sass(
-      {
-        outputStyle: "expanded"
-      })
-    .on("errorProd", sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.styles.prod))
-    .pipe(browserSync.stream());
-});
-
-gulp.task("imagesProd", function() {
-  return gulp.src(paths.images.src)
-    .pipe(gulp.dest(paths.images.prod))
+    .pipe(gulpif(isProduction, gulp.dest(paths.images.dest)))
+    .pipe(gulpif(!isProduction, gulp.dest(paths.images.prod)))
     .pipe(browserSync.stream());
 });
 
@@ -103,8 +79,6 @@ gulp.task("watch", function () {
   gulp.watch(["src/styles/**/*.scss", "src/html/**/*.html"], ["html", "fonts", "images", "sass"]);
 });
 
-gulp.task("dev", ["html", "fonts", "sass", "images"]);
-
-gulp.task("build", ["htmlProd", "fontsProd", "sassProd", "imagesProd"]);
+gulp.task("build", ["html", "fonts", "sass", "images"]);
 
 gulp.task("default", ["sass", "html", "fonts", "images", "watch"]);
